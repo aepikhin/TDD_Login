36250ad8c3f708f34a8924110039b093caf4d91f
Anton Epikhin <unb_toni@yahoo.com>
2015-12-10 21:24:01 +0100

    Some tests implemented. More TODO.

--- TDD_Login/Tests_Login/TestDBPruebas.cs
+++ TDD_Login/Tests_Login/TestDBPruebas.cs
@@ -6,9 +6,98 @@ namespace Tests_Login
     [TestClass]
     public class TestDBPruebas
     {
-        [TestMethod]
-        public void TestMethod1()
+        DBPruebas dbInstancia;
+
+        private DBPruebas fabrica()
         {
+            if (dbInstancia == null)
+                dbInstancia = new DBPruebas();
+            return dbInstancia;
+        }
+
+        [TestMethod()]
+        public void DBPruebasTest()
+        {
+            DBPruebas db = fabrica();
+            Assert.IsNotNull(db);
+        }
+
+        [TestMethod()]
+        public void TestValida()
+        {
+            DBPruebas db = fabrica();
+
+            Assert.IsTrue(db.valida("admin1", "pass1"));
+            Assert.IsFalse(db.valida("admin1", "pass1".GetHashCode()));
+            Assert.IsFalse(db.valida("admin1", "pass2"));
+
+            Assert.IsTrue(db.valida("admin1", "pass1"));
+            Assert.IsFalse(db.valida("admin2", "pass1"));
+
+            Assert.IsFalse(db.valida("usuarioBloqueado1", "pass1"));
+        }
+
+        [TestMethod()]
+        public void TestCreaUsuario()
+        {
+            DBPruebas db = fabrica();
+
+            Assert.IsTrue(db.creaUsuario("userTest", "Nombre 1", "Apellidos 1", "pass1", TipoUsuario.ADMINISTRADOR));
+            Assert.IsFalse(db.creaUsuario("userTest", "Nombre 2", "Apellidos 2", "pass2", TipoUsuario.BECARIO));
+        }
+
+        [TestMethod()]
+        public void TestCambiaTipo_TestEsAdministrador()
+        {
+            DBPruebas db = fabrica();
+            String userName = "userTest2";
+            db.creaUsuario(userName, "Nombre 2", "Apellidos 2", "pass2", TipoUsuario.ADMINISTRADOR);
+            
+            Assert.IsTrue(db.esAdministrador(userName));
+            db.cambiaTipo(userName, TipoUsuario.BECARIO);
+            Assert.IsFalse(db.esAdministrador(userName));
+        }
+
+        [TestMethod()]
+        public void TestCambiaContrasena()
+        {
+            DBPruebas db = fabrica();
+            String userName = "userTest3";
+            db.creaUsuario(userName, "Nombre 3", "Apellidos 3", "pass3", TipoUsuario.ADMINISTRADOR);
+            
+            Assert.IsTrue(db.cambiaContrasena(userName, "pass3", "passNueva"));
+            Assert.IsFalse(db.cambiaContrasena(userName, "pass3", "passNueva"));
+        }
+
+        [TestMethod()]
+        public void TestDesbloqueaUsuario()
+        {
+            DBPruebas db = fabrica();
+            String userName = "userTest3";
+            db.creaUsuario(userName, "Nombre 3", "Apellidos 3", "pass3", TipoUsuario.ADMINISTRADOR);
+
+            //TODO
+        }
+
+        [TestMethod()]
+        public void TestBorrarLog()
+        {
+            DBPruebas db = fabrica();
+            String userName = "userTest3";
+            db.creaUsuario(userName, "Nombre 3", "Apellidos 3", "pass3", TipoUsuario.ADMINISTRADOR);
+
+            //TODO
+        }
+
+        [TestMethod()]
+        public void TestGrabaEvento()
+        {
+            DBPruebas db = fabrica();
+            String userName = "userTest3";
+            db.creaUsuario(userName, "Nombre 3", "Apellidos 3", "pass3", TipoUsuario.ADMINISTRADOR);
+
+            //TODO
         }
     }
+    
 }
--- TDD_Login/Tests_Login/TestUsuario.cs
+++ TDD_Login/Tests_Login/TestUsuario.cs
@@ -6,13 +6,81 @@ namespace Tests_Login
     [TestClass]
     public class TestUsuario
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            Usuario user = new Usuario("user1", "Nombre 1", "Apellidos 1", "pass1", TipoUsuario.ADMINISTRADOR);
+        Usuario userInstancia;
 
+        private Usuario fabrica()
+        {
+            if (userInstancia == null)
+                userInstancia = new Usuario("user1", "Nombre 1", "Apellidos 1", "pass1", TipoUsuario.ADMINISTRADOR, 3);
+            return userInstancia;
+        }
+
+        [TestMethod()]
+        public void UserTest()
+        {
+            Usuario user = fabrica();
+            Assert.IsNotNull(user);
+        }
+
+        [TestMethod]
+        public void TestNombre()
+        {
+            Usuario user = fabrica();
             Assert.IsTrue(user.Nombre, "Nombre 1");
             Assert.IsFalse(user.Nombre, "Nombre 2");
+            user.Nombre = "Nombre 2";
+            Assert.IsTrue(user.Nombre, "Nombre 2");
+            Assert.IsFalse(user.Nombre, "Nombre 1");
+        }
+
+        [TestMethod]
+        public void TestApellidos()
+        {
+            Usuario user = fabrica();
+            Assert.IsTrue(user.Apellidos, "Apellidos 1");
+            Assert.IsFalse(user.Apellidos, "Apellidos 2");
+            user.Apellidos = "Apellidos 2";
+            Assert.IsTrue(user.Apellidos, "Apellidos 2");
+            Assert.IsFalse(user.Apellidos, "Apellidos 1");
+        }
+
+        [TestMethod]
+        public void TestUsername()
+        {
+            Usuario user = fabrica();
+            Assert.IsTrue(user.UserName, "user1");
+            Assert.IsFalse(user.UserName, "user2");
+        }
+
+        [TestMethod]
+        public void TestPassword()
+        {
+            Usuario user = fabrica();
+            Assert.IsFalse(user.Password, "pass1");
+            Assert.IsTrue(user.Password, "pass1".GetHashCode());
+            Assert.IsFalse(user.Password, "pass2".GetHashCode());
+        }
+
+        [TestMethod]
+        public void TestTipoUsuario()
+        {
+            Usuario user = fabrica();
+            Assert.IsTrue(user.TipoUsuario, TipoUsuario.ADMINISTRADOR);
+            Assert.IsFalse(user.TipoUsuario, TipoUsuario.BECARIO);
+            user.TipoUsuario = TipoUsuario.BECARIO;
+            Assert.IsTrue(user.TipoUsuario, TipoUsuario.BECARIO);
+            Assert.IsFalse(user.TipoUsuario, TipoUsuario.ADMINISTRADOR);
+        }
+
+        [TestMethod]
+        public void TestNumeroIntentosLogin()
+        {
+            Usuario user = fabrica();
+            Assert.IsTrue(user.NumeroIntentosLogin, 3);
+            Assert.IsFalse(user.NumeroIntentosLogin, 5);
+            user.NumeroIntentosLogin = 5;
+            Assert.IsTrue(user.NumeroIntentosLogin, 5);
+            Assert.IsFalse(user.NumeroIntentosLogin, 3);
         }
     }
 }
